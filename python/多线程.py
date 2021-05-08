c0bfb4da8869360c0c61c5471f612fbe23c591bf
yaobc <yaobc@ustc-it.com>
2021-05-08 11:10:27 +0800

    python多线程补充说明

--- python/多线程.py
+++ python/多线程.py
@@ -10,3 +10,8 @@ def newthread(i):
     print(i)
 
 test()
+
+
+#setDaemon(true)，主线程结束时强制子线程结束
+#setDaemon(false)，主线程结束时,子线程还会继续执行直到结束
+#join，主线程执行到子线程时阻塞，等到所有子线程执行完毕后再继续执行主线程，可以设置timeout，单位是seconds
